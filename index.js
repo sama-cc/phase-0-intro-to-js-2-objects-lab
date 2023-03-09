0c134a58028a9f67d0c124295da50efc5c853165
Sam <sam.camhi@gmail.com>
2023-03-08 19:26:25 -0500

    Passed Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "1234 This House",
+}
+
+function updateEmployeeWithKeyAndValue(emp, key, value) {
+    const newEmp = { ...emp };
+
+    newEmp[key] = value;
+
+    return newEmp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value) {
+    emp[key] = value;
+
+    return emp;
+}
+
+function deleteFromEmployeeByKey(emp, key) {
+    const newDelEmp = { ...emp };
+
+    delete newDelEmp[key];
+
+    return newDelEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(emp, key) {
+    delete emp[key];
+
+    return emp;
+}
+
+
+
